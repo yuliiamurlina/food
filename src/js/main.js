f7a444d7db06218598ca6e229682e22c8da42793
Yuliia Murlina <y4liamur@gmail.com>
2023-11-25 15:57:28 +0100

    new

--- src/js/main.js
+++ src/js/main.js
@@ -318,9 +318,115 @@ window.addEventListener("DOMContentLoaded", () => {
     nextButton = slider.querySelector(".offer__slider-next"),
     slides = slider.querySelectorAll(".offer__slide"),
     total = slider.querySelector("#total"),
-    current = slider.querySelector("#current");
-  let slideIndex = 1;
+    current = slider.querySelector("#current"),
+    slidesWrapper = slider.querySelector(".offer__slider-wrapper"),
+    slidesInner = slidesWrapper.querySelector(".offer__slider-inner"),
+    width = window.getComputedStyle(slidesWrapper).width;
+  let slideIndex = 1,
+    offset = 0;
 
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
+  } else {
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
+  }
+
+  slidesInner.style.width = 100 * slides.length + "%";
+  slidesInner.style.display = "flex";
+  slidesInner.style.transition = ".5s all";
+
+  slidesWrapper.style.overflow = "hidden";
+
+  slides.forEach((slide) => (slide.style.width = width));
+
+  slider.style.position = "relative";
+
+  function checkCurrentPosition() {
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
+  function setDot() {
+    dots.forEach((dot) => (dot.style.opacity = ".5"));
+    dots[slideIndex - 1].style.opacity = 1;
+  }
+
+  function cutNotNumbers(str) {
+    return +str.replace(/\D/g, "");
+  }
+
+  const indicators = document.createElement("ol"),
+    dots = [];
+  indicators.classList.add("carousel-indicators");
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("li");
+    dot.setAttribute("data-slide-to", i + 1);
+    dot.classList.add("dot");
+    dots.push(dot);
+
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+
+    indicators.append(dot);
+  }
+
+  nextButton.addEventListener("click", () => {
+    if (offset == cutNotNumbers(width) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += cutNotNumbers(width);
+    }
+    slidesInner.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    checkCurrentPosition();
+
+    setDot();
+  });
+
+  prevButton.addEventListener("click", () => {
+    if (offset == 0) {
+      offset = cutNotNumbers(width) * (slides.length - 1);
+    } else {
+      offset -= cutNotNumbers(width);
+    }
+    slidesInner.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    checkCurrentPosition();
+    setDot();
+  });
+
+  dots.forEach((dot) => {
+    dot.addEventListener("click", (e) => {
+      const slideTo = e.target.getAttribute("data-slide-to");
+      slideIndex = slideTo;
+      offset = cutNotNumbers(width) * (slideTo - 1);
+      slidesInner.style.transform = `translateX(-${offset}px)`;
+      setDot();
+      checkCurrentPosition();
+    });
+  });
+
+  /* FIRST OPTION
   if (slides.length < 10) {
     total.textContent = `0${slides.length}`;
   } else {
@@ -357,4 +463,29 @@ window.addEventListener("DOMContentLoaded", () => {
 
   prevButton.addEventListener("click", () => plusSlide(-1));
   nextButton.addEventListener("click", () => plusSlide(1));
+  */
+
+  //Calc
+
+  const resultCalc = document.querySelector(".calculating__result span");
+  let sex, height, weight, age, ratio;
+
+  function calcTotal() {
+    if (!sex || !height || !weight || !age || !ratio) {
+      resultCalc.textContent = "_____";
+      return;
+    }
+
+    if (sex === "female") {
+      resultCalc.textContent =
+        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio;
+    } else {
+      resultCalc.textContent =
+        (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio;
+    }
+  }
+
+  calcTotal();
+
+  function getStaticInformation() {}
 });
